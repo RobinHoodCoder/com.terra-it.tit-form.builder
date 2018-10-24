2830cb4dd05f6ad000cce1661b47ff5f1a9ca429
Terrra-Robin <robin@terra-it.com>
2018-10-24 17:32:31 +0200

    Bugs gefixed met volgende formuliersectie die niet wilt openen na invullen laatste formulier item..

--- public/js/titform.js
+++ public/js/titform.js
@@ -1,40 +1,62 @@
 window.onload = function() {
 
-    // Maak verzend knop disabled tot form open is.
+    /*------------------------
+    Algemene formulierfuncties!!
+    --------------------*/
+    $( "select" ).change(function() {
+        var selected = $(this).children(":selected");
+        var otherfield = $(selected).parent().parent().next('.form-input-group').find('.otherwise');
+        if (selected.hasClass('open-new-input')){
+            $(otherfield).fadeIn( "fast" );
+        }else{
+            $(otherfield).fadeOut( "fast" );
+        }
+    });
+    // Extra veldje toevoegen bij "anders, namelijk:"
+    $('.otherwise').hide();
+    $('input.open-new-input').on('click', function(){
+        var nextField = $(this).parent().parent().parent().next('.otherwise');
+        if($(this).hasClass('open-new-input')){
+            $(nextField).fadeIn( "fast" );
+        }else{
+            $(otherField).fadeOut( "fast" );
+        }
+    });
 
 
-    //Zet alle velden op disabled behalve de eerste
 
 
 
 
-    
-
+    /*------------------------
+   Prijsbereken formulierfuncties!!
+   --------------------*/
     $('.multipart-form').each(function(){
 
-        // Zet alles op disabled...
-
-        //1. Dit zijn de input velden die die disabled moet zijn..
+        //1. Dit zijn de input velden die die disabled moeten zijn..
         inputs = $(this).find('input,select,textarea,button' );
 
-        //2. Vind het eerste inputveld..
-        firstInputGroup = $(this).find('.first-item .form-input-group');
-
-
-        //3. Disable alles..
+        //2. Disable alles..
         $(this).find(inputs.addClass('disabled').attr('disabled',true));
 
-        //4. Maak het eerste inputveld invulbaar..
-        // Alles behalve het eerste veld
-        setTimeout(function(){
-            firstInputGroup.first().find('input').removeClass('disabled').attr('disabled',false);
-        },200);
+
+
+        //3. Vind het eerste inputveld van elke form item
+        $(this).find('.form-item').each(function(itemcount, formItem){
+            var firstInputGroup = $(formItem).find('.form-input-group').first();
+            console.log(firstInputGroup);
+
+            firstInputGroup.each(function (itemcount, firstInputGroup) {
+                $(firstInputGroup).find('.field-item').removeClass('disabled').attr('disabled',false);
+            });
+            // $(firstInputGroup).find('.field-item').removeClass('disabled').attr('disabled',false);
+        });
 
         /*
         * Maak veld na checkboxes actief na het aanklikken van 1 item.
         * Na uitklikken maak je het weer inactief
         * */
-
+        // -----------
         // Zet een class op de aangeklikte checkbox!
         $(this).find('.checkbox').change(function(){
             $(this).parent().toggleClass('checked-label');
@@ -45,33 +67,29 @@ window.onload = function() {
             $(this).children('.form-input-group').last().addClass('last-form-group');
         });
 
-        console.group('Checkboxes checkerr');
+        //Check of een checkboxveld veranderd.
         $(this).find(".contain-cbox").change(function() {
             var containerBox =  $(this);
             var foundItem = $(this).find('.checked-label');
             var nextGroup = containerBox.parents().next('.form-input-group');
             if (foundItem.length === 0){
-                console.log('Geen gechecked items!!!');
+                // console.log('Geen gechecked items!!!');
                 nextField = nextGroup.find('.field-item');
                 nextField.addClass('disabled');
                 nextField.attr('disabled', true);
             }
             if (foundItem.length >= 1){
-                console.log('Er zijn gecheckede items!!!');
+                // console.log('Er zijn gecheckede items!!!');
                 nextGroup.find('.field-item').removeClass('disabled');
                 nextGroup.find('.field-item').attr('disabled', false);
             }
         });
-        console.groupEnd('Checkboxes checkerr');
-        // function openNextFormItem(){alert('JAaaa er is eer een aangeklikt!!! :D:D:D');}
-        // function closeNextFormItem(){alert('Er zijn geeen vinkjes meer aangekliikt :(:(');}
 
-            /*
-       * Als er iets is veranderd in aeeen niet-checkbox veld, maak dan het volgende item actief.
-       * */
+        /*
+   * Als er iets is veranderd in aeeen niet-checkbox veld, maak dan het volgende item actief.
+   * */
 
         $(this).find(".field-item" ).not('.checkbox').change(function() {
-
             //$Nexttfield is alle inputvelden in het volgende field item!!
             var nextField = $(this).parents('.form-input-group').next('.form-input-group').find('.field-item');
             console.log($(nextField));
@@ -94,7 +112,7 @@ window.onload = function() {
             var lastItem = $(this).find('.field-item');
 
             if($(lastItem).hasClass('disabled')) {
-                //DOE NIETS!
+                //DOE NIETS als laatste element nog niet aanklikbaar is!!
             }else{
                 // MAak de volgende sectie van het formulier zichtbaar!!
                 $(this).removeClass('last-form-group');
@@ -103,96 +121,26 @@ window.onload = function() {
 
                 var nextFormStep =  $(this).parents('.done').next('.form-item');
 
-
-
-
-                // MAak het eerste veld van de volgende sectie invulbaar!!
-
-
-                var nextFormItem = $('.active-step');
-
+                // MAak het volgende stap in formulier zichtbaar
                 $(nextFormStep).slideDown(150, "swing").addClass('active-step');
+                var activeStep = $('.active-step');
+                // $(activeStep).find('.field-item').removeClass('disabled').attr('disabled',false);
             }
 
         });
 
 
-
-        //Maak eerste veld van volgende stap invulbaar...
-
-
-        // Extra veldje toevoegen als dit nodig is.
-        $('.otherwise').hide();
-        $('input.open-new-input').on('click', function(){
-            var nextField = $(this).parent().parent().parent().next('.otherwise');
-            if($(this).hasClass('open-new-input')){
-                $(nextField).fadeIn( "fast" );
-            }else{
-                $(otherField).fadeOut( "fast" );
-            }
-        });
-
-        $( "select" ).change(function() {
-            var selected = $(this).children(":selected");
-            var otherfield = $(selected).parent().parent().next('.form-input-group').find('.otherwise');
-            if (selected.hasClass('open-new-input')){
-                $(otherfield).fadeIn( "fast" );
-            }else{
-                $(otherfield).fadeOut( "fast" );
-            }
-        });
-
-        //Maak button niet meer disabled bij aanpassen ALLERlaatste inputveld
+        //Maak verzendbutton niet meer disabled bij aanpassen ALLERlaatste inputveld
         var lastInput = $(this).find('.calculate-item input').last();
-        $(lastInput).change(function(){
+        $(this).find(lastInput).change(function(){
             $('.disabled-button').prop('disabled', false); //makes it enabled
             $('form button').removeClass('disabled-button');
         });
-
-
     });
-
-
-
-
-
-
-
-
     // $(formInputs.items).not(':first').not('.checkbox').each(function(index, value){
     //     $(value).prop('disabled', true);
     //     $(value).addClass('disabled');
     // });
-    //Maak eerst volgende veld editable na change
-
-    // var log = $( ".field-item" ).has('checkbox');
-
-
-
-
-
-
-
-
-
-
-
-   /********
-  *     Maak volgende veld actief na veranderen van een radiobutton
-  * */
-
-
-
-
-
-
-
-
-    // als je ergens klikt op het document, voer dan deze check uit:
-
-
-
-
     // $( ".number" ).hover(function() {
     //     var nextField = $(this).parents('form').find('.disabled:first');
     //
@@ -203,25 +151,4 @@ window.onload = function() {
     // });
 
     // Maak verzend knop disabled tot form open is.
-
-
-
-
-
-    // if (formItems > 2){
-    //     $('.form-item').parent().css({
-    //         'display': 'flex',
-    //         'flex-wrap': 'wrap'
-    //     });
-    // }
-
-    // Open 2e form blok
-
-
-
-
-
-
-
-
 };
